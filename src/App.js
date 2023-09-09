f405c7e503dfa70b624ef2b9415666b1512234f5
kaushal <megamindruler@gmail.com>
2023-09-10 05:06:05 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import DemoComponent from './DemoComponent';
 
+//import DemoComponent from './DemoComponent';
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <DemoComponent />
-      </header>
-    </div>
-  );
-}
+        <nav className='App-nav'>
 
+          <ul>
+          <li className='App-logo'>
+             ResumeCraftr
+            </li><li>
+             Create Resume
+            </li>
+            <li>
+             Home 
+            </li>
+            <li>
+             Network
+            </li>
+            <li className='App-nav-right'>
+            Login / SignUp
+            </li>
+          </ul>
+        </nav>
+        {/* <DemoComponent /> */}
+      </header>
+      <section></section>
+      
+      </div>)
+}
 export default App;
